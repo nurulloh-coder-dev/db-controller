75055770fd0c4b73b6fc599d618aa145cb5a16e8
developer <nurilloh54@gmail.com>
2026-05-10 17:20:56 +0500

    mail sender alternation

--- src/main/java/com/example/service/EmailService.java
+++ src/main/java/com/example/service/EmailService.java
@@ -1,58 +1,69 @@
 package com.example.service;
 
-import jakarta.mail.internet.MimeMessage;
-import lombok.AllArgsConstructor;
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@AllArgsConstructor
 public class EmailService {
-    private final JavaMailSender mailSender;
 
-    @Async
-    public void sendEmailAboutCreationAcc(String userEmail, String text, String subject) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(userEmail);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(text);
-        mailSender.send(mailMessage);
+    private final Resend resend;
+
+    @Value("${resend.from-email}")
+    private String fromEmail;
+
+    public EmailService(@Value("${resend.api-key}") String apiKey) {
+        this.resend = new Resend(apiKey);
     }
 
     @Async
+    @SneakyThrows
+    public void sendEmailAboutCreationAcc(String userEmail, String text, String subject) {
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from(fromEmail)
+                .to(userEmail)
+                .subject(subject)
+                .text(text)
+                .build();
+        resend.emails().send(params);
+    }
+
+    @Async
+    @SneakyThrows
     public void sendEmailAboutUpdate(String userEmail, String text, Boolean sendEmail) {
         if (sendEmail) {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(userEmail);
-            mailMessage.setSubject("Update to your profile");
-            mailMessage.setText(text);
-            mailSender.send(mailMessage);
-            log.info("Email is sent successfully to email : {} on update to db user", userEmail);
+            CreateEmailOptions params = CreateEmailOptions.builder()
+                    .from(fromEmail)
+                    .to(userEmail)
+                    .subject("Update to your profile")
+                    .text(text)
+                    .build();
+            resend.emails().send(params);
+            log.info("Email sent to: {}", userEmail);
         }
     }
 
-    @SneakyThrows
     @Async
+    @SneakyThrows
     public void sendResetLink(String email, String rawToken) {
         String htmlBody = """
-                    <p>Click the link below to reset your password:</p>
-                    <a href="https://yourapp.com/reset-password?token=%s">Reset Password</a>
-                    <p>This link expires in 15 minutes.</p>
-                    <p>If you did not request this, ignore this email.</p>
+                <p>Click the link below to reset your password:</p>
+                <a href="https://coder12345-web.github.io/db-controller-front/login/reset-password.html?token=%s">Reset Password</a>
+                <p>This link expires in 15 minutes.</p>
+                <p>If you did not request this, ignore this email.</p>
                 """.formatted(rawToken);
 
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-        helper.setTo(email);
-        helper.setSubject("Password Reset Request");
-        helper.setText(htmlBody, true);
-        mailSender.send(message);
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from(fromEmail)
+                .to(email)
+                .subject("Password Reset Request")
+                .html(htmlBody)
+                .build();
+        resend.emails().send(params);
     }
 }
